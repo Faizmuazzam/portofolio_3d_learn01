411ddca3193bd2c63cba41f58a619adf5bbfce40
Faiz Muazzam <65695747+Faizmuazzam@users.noreply.github.com>
2023-11-28 22:23:40 +0700

    Initial commit

---
+++ src/components/Experience.jsx
@@ -0,0 +1,51 @@
+import { OrbitControls, Sky, ContactShadows, SpotLight, Environment } from "@react-three/drei";
+import { Avatar } from "./Avatar";
+import { Office } from "./Office";
+import { useControls } from "leva";
+import { BoxGeometry } from "three";
+
+export const Experience = () => {
+  const { animation } = useControls({
+    animation: {
+      value: 'Typing',
+      options: ["Typing", "Falling", "Standing"]
+    }
+  })
+  return (
+    <>
+      <OrbitControls />
+
+      <Sky />
+      <Environment
+        background
+        blur={0}
+        preset="sunset"
+      />
+
+      <Office />
+      <group position-y={0}>
+        <Avatar animation={animation} />
+        <ContactShadows
+          opacity={0.45}
+          scale={10}
+          blur={1}
+          far={10}
+          resolution={256}
+          color={'#000000'}
+        />
+        {/* {animation === "Typing" && (
+          <mesh scale={[0.8, 0.5, 0.8]} position={[0, 0.25, 0]}>
+            <boxGeometry />
+            <meshStandardMaterial color={'red'} />
+          </mesh>
+        )} */}
+
+        {/* <mesh scale={5} rotation-x={-Math.PI * 0.5} position-y={-0.001}>
+          <planeGeometry />
+          <meshStandardMaterial color={'yellow'} />
+        </mesh> */}
+      </group>
+      {/* <ambientLight intensity={0.5} /> */}
+    </>
+  );
+};
